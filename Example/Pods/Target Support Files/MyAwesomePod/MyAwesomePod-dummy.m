c7666e229793ed444d06199586734e053f68f3f8
Gagik Mkrtchyan <gagik.mkrtchyan@acba.am>
2025-02-27 15:52:41 +0400

    first commit

---
+++ Example/Pods/Target Support Files/MyAwesomePod/MyAwesomePod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MyAwesomePod : NSObject
+@end
+@implementation PodsDummy_MyAwesomePod
+@end
---
+++ MyAwesomePod/Classes/ReplaceMe.swift
@@ -0,0 +1,8 @@
+
+public class AwesomeFeature {
+    public init() {}
+
+    public func sayHello() -> String {
+        return "Hello from MyAwesomePod!"
+    }
+}
